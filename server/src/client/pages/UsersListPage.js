939ed66f2a9f0eb40f9cf0963f708a1f7253a0dc
Brian Yingling <brian@jet.com>
2017-12-09 12:04:27 -0500

    hydrating the client store with data fetched from the server

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -2,7 +2,7 @@ import React, {Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
 
-class UsersList extends Component {
+class UsersListPage extends Component {
     
     componentDidMount() {
         this.props.fetchUsers();
@@ -66,4 +66,7 @@ function loadData(store) {
 
 export { loadData };
 
-export default connect(mapStateToProps, { fetchUsers })(UsersList);
+export default {
+    loadData,
+    component: connect(mapStateToProps, { fetchUsers })(UsersListPage)
+};
