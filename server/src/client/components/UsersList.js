207132340fb5ee0fad3b192d55622ec1c272f75e
Brian Yingling <brian@jet.com>
2017-12-09 11:16:38 -0500

    comments on how ssr works

--- server/src/client/components/UsersList.js
+++ server/src/client/components/UsersList.js
@@ -32,6 +32,34 @@ function mapStateToProps(state) {
     }
 }
 
+// Why are we making use of the store?
+// we're not using the normal connect function
+// to use action creators here. Why?
+// Connect fn works in a normal app by communicating
+// with the provider tag, which has a reference 
+// to the store.
+// We are not using the connect tag here because
+// connect only works through communication with the
+// Provider.
+// We need to use redux and collect data before 
+// rendering our application at all.
+// So, no Provider tag, no props because we have not
+// yet rendered our application which means we cannot
+// use the connect tag.
+
+// Why are we using the dispatch function here?
+// Since we cannot use the connect tag, we are 
+// working with redux manually by calling the action 
+// creator ourselves so that the dispatch function can 
+// pipe that through middleware and then through reducers.
+
+// Why is redux involved in this data fetching process?
+// All loadData functions then dispatch actions which 
+// then populate the store. The store is then full of data.
+// This all happens before the page is rendered and sent
+// back tot the client. A series of promises are called,
+// which are the loadData functions, then once they complete
+// they populate the store before it's sent back to the client.
 function loadData(store) {
     return store.dispatch(fetchUsers());
 }
